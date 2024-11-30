0e55091265dd89f3f5b90e510c296f0d520d39b1
Olena Poplavko <alonaforce@gmail.com>
2024-11-30 01:47:35 +0100

    Cat Images

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ const response = await fetch(apiUrl);
 const data = await response.json();
 return data;
 }
-  
+  function getCatImage(description) {
+    const catImages = {
+        "clear sky":"",
+        "few clouds":"",
+        "rain":"",
+        "snow":"",
+        "default":""
+    };
+    return catImages[description]|| catImages["default"];
+  };
 document.getElementById("catGetWeather").addEventListener("click", async function() {
     const catCityInput = document.getElementById("catCityName").value.trim();
     const weatherData = await fetchCatWeather(catCityInput);
+ const catImageUrl = getCatImage(weatherData.weather[0].description);
+
 document.getElementById("catWeatherDisplay").innerHTML = 
 `<h2>Weather in ${weatherData.name}</h2>
 <p>Temperature: ${weatherData.main.temp}°C</p>
-<p>Description: ${weatherData.weather[0].description}</p>`;    
+<p>Description: ${weatherData.weather[0].description}</p>
+<img src="${catImageUrl}" alt="Weather Cat" style="max-width: 300px; border-radius: 10px;"/>`;    
 
 });
